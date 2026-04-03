5eb1a2efbead1cd9b60f08fca6bac67e6dbaf1a1
artmatique <mekartronique@gmail.com>
2026-04-03 19:50:13 +0000

    main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.querySelector('.search-box input');
+    
+    searchInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            alert("Recherche ArtMatique pour : " + searchInput.value);
+        }
+    });
 
+    console.log("ArtMatique Pro est prêt !");
+});
